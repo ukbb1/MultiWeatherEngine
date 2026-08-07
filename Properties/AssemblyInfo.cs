28e8e94369a2ddcb8a4cee64870f00bca3d30206
ukbb1 <poki12342024@163.com>
2026-08-07 14:11:40 +0800

    Add Properties/AssemblyInfo.cs

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyCompany("MultiWeatherEngine")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyFileVersion("2.0.98.0")]
+[assembly: AssemblyInformationalVersion("2.0.98")]
+[assembly: AssemblyProduct("MultiWeatherEngine")]
+[assembly: AssemblyTitle("MultiWeatherEngine")]
+[assembly: AssemblyVersion("2.0.98.0")]
